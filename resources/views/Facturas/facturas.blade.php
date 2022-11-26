ecac410834f2dd531f031727b3a7226fb0718217
Esteban <jecastro648@misena.edu.co>
2022-11-26 17:42:55 -0500

    Modulo factura

--- resources/views/Facturas/facturas.blade.php
+++ resources/views/Facturas/facturas.blade.php
@@ -62,8 +62,8 @@
                 <th>iva</th>
                 <th>Total</th>
                 <th>Fecha</th>
-                <th></th>
-                <th></th>
+                <th>Editar</th>
+                <th>PDF</th>
             </tr>
         </thead>
         <tbody id="myTable">
@@ -83,9 +83,9 @@
                 <td data-label="iva">{{ $factura->iva }}</td>
                 <td data-label="Total">{{ $factura->total }}</td>
                 <td data-label="Fecha">{{ $factura->fecha }}</td>
-                <td data-label="Editar"><a href="{{ route('facturas.edit', $factura->num_factura) }}">Editar</a> </td>
+                <td data-label="Editar"><a href="{{ route('facturas.edit', $factura->num_factura) }}"><i class="bi bi-pencil-square"></i></a> </td>
                 <td>
-                    <a href="#">
+                    <a href="{{ route('facturaPDF.print') }}" target="_blank">
                         <svg class="svg" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-printer" viewBox="0 0 16 16">
                             <path d="M2.5 8a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z" />
                             <path d="M5 1a2 2 0 0 0-2 2v2H2a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h1v1a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-1h1a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-1V3a2 2 0 0 0-2-2H5zM4 3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v2H4V3zm1 5a2 2 0 0 0-2 2v1H2a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1v-1a2 2 0 0 0-2-2H5zm7 2v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1z" />
