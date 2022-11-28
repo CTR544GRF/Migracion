a028d741c021e62ff371dd957a27acbd34054088
Camilo Diaz <camilo1003diaz@gmail.com>
2022-11-27 22:59:01 -0500

    estilos contacto

--- resources/views/email/contactanos.blade.php
+++ resources/views/email/contactanos.blade.php
@@ -6,40 +6,66 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Email</title>
+    <style>
+        * {
+            box-sizing: border-box;
+        }
+
+        .campos {
+            width: 100%;
+            display: flex;
+            align-items: center;
+            gap: 10px;
+            justify-content: flex-start;
+            padding: 0 0 0 20px;
+        }
+    </style>
 </head>
 
 <body>
-    <h1>Informacion de Contacto</h1>
-    <p>
-        <strong>
-            <h2>Nombre:</h2>
-        </strong>
-        {{$contacto['nombre']}}
-    </p>
-    <p>
-        <strong>
-            <h2>Telefono Celular:</h2>
-        </strong>
-        {{$contacto['celular']}}
-    </p>
-    <p>
-        <strong>
-            <h2>Correo Electronico:</h2>
-        </strong>
-        {{$contacto['correo']}}
-    </p>
-    <p>
-        <strong>
-            <h2>Ciudad de Contacto:</h2>
-        </strong>
-        {{$contacto['ciudad']}}
-    </p>
-    <p>
-        <strong>
-            <h2>Descripcion:</h2>
-        </strong>
-        {{$contacto['mensaje']}}
-    </p>
+    <div class="container" style="
+    margin: auto;
+    width: 60%;
+    background: #ecf6f5;
+    border-radius: 20px;
+    text-align: center;
+    padding: 10px;
+">
+        <h1>Informacion de Contacto</h1>
+        <div class="campos">
+            <h2>
+                <strong>Nombre:</strong>
+            </h2>
+            <h3>{{$contacto['nombre']}}</h3>
+        </div>
+        <div class="campos">
+            <h2>
+                <strong>Telefono Celular:</strong>
+            </h2>
+            <h3>{{$contacto['celular']}}</h3>
+        </div>
+
+        <div class="campos">
+            <h2>
+                <strong>Correo Electronico:</strong>
+            </h2>
+            <h3>{{$contacto['correo']}}</h3>
+        </div>
+
+        <div class="campos">
+            <h2>
+                <strong>Ciudad de Contacto:</strong>
+            </h2>
+            <h3>{{$contacto['ciudad']}}</h3>
+        </div>
+
+        <div class="campos">
+            <h2>
+                <strong>Descricion:</strong>
+            </h2>
+            <h3>{{$contacto['mensaje']}}</h3>
+        </div>
+    </div>
 
 </body>
 
